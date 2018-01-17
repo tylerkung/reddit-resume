d814bc7c82b0248a6cf5061b90c4cd113e044915
Tyler Kung <tylerk@Tyler-Macbook-Pro.local>
2018-01-17 14:46:16 -0800

    Work on temp.html, making votes work

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,26 @@ $(document).ready(function(){
         $(this).toggleClass("collapsed");
         $(this).parent().children(".expand-box").toggleClass("hidden");
     });
+    $(".arrow.up").click(function(){
+        if ($(this).hasClass("voted")){
+            $(this).removeClass("voted").addClass("unvoted");
+            $(this).parent(".vote").removeClass("upvoted").addClass("unvoted");
+        }
+        else{
+            $(this).addClass("voted");
+            $(this).parent(".vote").removeClass("unvoted downvoted").addClass("upvoted");
+            $(this).siblings(".arrow.down").removeClass("voted");
+        }
+    });
+    $(".arrow.down").click(function(){
+        if ($(this).hasClass("voted")){
+            $(this).removeClass("voted").addClass("unvoted");
+            $(this).parent(".vote").removeClass("downvoted").addClass("unvoted");
+        }
+        else{
+            $(this).addClass("voted");
+            $(this).parent(".vote").removeClass("unvoted upvoted").addClass("downvoted");
+            $(this).siblings(".arrow.up").removeClass("voted");
+        }
+    });
 });
