69ad27ffa926b52d16d64bd928d0c61e0298567c
Tyler Kung <tylerk@Tyler-Macbook-Pro.local>
2018-07-17 00:13:22 -0700

    adding new portfolio

---
+++ _Portfolio2.0/js/scripts.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+    $(".skill").each(function(){
+        $(this).find('.skill-bar').css(
+            "width", $(this).attr('data-skill') + '%'
+        );
+    });
+
+
+    window.sr = ScrollReveal({
+        reset: false
+    });
+    var fadeUp = {
+        distance: '20px',
+        origin: 'bottom',
+        opacity: 0,
+        scale: 1,
+        viewFactor: 0.2,
+        easing: 'cubic-bezier(0,.5,.5,1)',
+        afterReveal: function(domEl){
+            $(domEl).addClass("animate");
+        }
+    }
+    sr.reveal('.nav a', {
+        distance: '20px',
+        origin: 'top',
+        opacity: 0,
+        scale: 1,
+        viewFactor: 0.2,
+        easing: 'cubic-bezier(0,.5,.5,1)',
+        delay: 400,
+    }, 300);
+    sr.reveal('.section-number', fadeUp);
+    sr.reveal('.lead-paragraph', {
+        distance: '5px',
+        opacity: 0,
+        scale: 1,
+        easing: 'ease-out',
+        delay: 200,
+        duration: 2000
+    });
+    sr.reveal('.em', {
+        opacity: 1,
+        scale: 1,
+        afterReveal: function(domEl){
+            $(domEl).addClass("animate");
+        }
+    });
+
+    sr.reveal('.timeline-item', {
+        distance: '0px',
+        opacity: 1,
+        scale: 1,
+        afterReveal: function(domEl){
+            $(domEl).addClass("animate");
+        }
+    }, 700);
+    var skillVal = fadeUp;
+    sr.reveal('.skill-col h3', fadeUp, 200);
+    sr.reveal('.skill-col .skill', {
+        distance: '0px',
+        scale: 1,
+        easing: 'cubic-bezier(0,.5,.5,1)',
+        afterReveal: function(domEl){
+            $(domEl).addClass("animate");
+        }
+    }, 100);
+    sr.reveal('.animate-all *', fadeUp, 300);
+    fadeUp.delay = 200;
+    sr.reveal('h1', fadeUp);
+    sr.reveal('h2', fadeUp);
+
+});
+//
+// function srClass(sr, val){
+//     sr.push
+// }
